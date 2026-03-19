eef1ea57924fe458fbbb6a252b190260c76934b9
AlexisPrograming <alexis97maga@gmail.com>
2026-03-19 14:19:34 -0400

    Switch to Neon PostgreSQL for Vercel deployment

--- focusflow/lib/db.ts
+++ focusflow/lib/db.ts
@@ -1,8 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
-import path from 'path'
-
-const dbPath = path.join(process.cwd(), 'dev.db')
+import { PrismaNeon } from '@prisma/adapter-neon'
 
 declare global {
   // eslint-disable-next-line no-var
@@ -10,7 +7,7 @@ declare global {
 }
 
 function createPrismaClient() {
-  const adapter = new PrismaBetterSqlite3({ url: `file:${dbPath}` })
+  const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! })
   return new PrismaClient({ adapter })
 }
 
